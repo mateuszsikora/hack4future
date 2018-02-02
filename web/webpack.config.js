7dceab5d42bed30706d77b3972fab81758c00297
Jakub Stasiak <kubafr2@gmail.com>
2018-02-02 22:59:35 +0100

    Web setup

---
+++ web/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(process.cwd(), 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-modules-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: true,
+              camelCase: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: path.join(process.cwd(), 'postcss.config.js'),
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputStyle: 'expanded',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        include: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(woff|ttf|eot|svg|otf)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+        loader: 'url-loader?limit=100000',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(process.cwd(), 'index.html'),
+    }),
+    new webpack.WatchIgnorePlugin([/scss\.d\.ts$/]),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV':
+        JSON.stringify(process.env.NODE_ENV) || 'development',
+    }),
+    new webpack.NamedModulesPlugin(),
+  ],
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+  },
+};
