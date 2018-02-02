ce5f30c562be3743b62f4ab588aec7b2e563198f
Dominik Markiewicz <theresq@gmail.com>
2018-02-02 19:54:18 +0100

    [mobile] add beacons support

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, DeviceEventEmitter } from 'react-native';
+
+import Beacons from 'react-native-beacons-manager'
+
+
+// Tells the library to detect iBeacons
+Beacons.detectIBeacons()
+
+
+
+// Start detecting all iBeacons in the nearby
+
+Beacons.startRangingBeaconsInRegion('REGION1').then(console.log, console.error)
 
 export default class App extends React.Component {
+    state = {}
+  componentDidMount(){
+  DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
+    console.log('Found beacons!', data.beacons)
+    this.setState({x: data.beacons});
+  })
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start dupa 123 working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{JSON.stringify(this.state.x)}</Text>
       </View>
     );
   }
