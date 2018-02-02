8c5dfdd4a38816dae2e7b2a3d02aa1d907e4da4b
Mateusz Sikora <mateusz.sikora@nokia.com>
2018-02-02 19:19:10 +0100

    collecting beacon from 1m area

--- gate/src/index.js
+++ gate/src/index.js
@@ -1,7 +1,16 @@
 const Bleacon = require('bleacon');
 
-Bleacon.on('discover', function(bleacon) {
-  console.log(bleacon);
+const MAX_ACCURACY = 1;
+
+const beacons = new Set();
+
+Bleacon.on('discover', (bleacon) => {
+  if (bleacon.accuracy < MAX_ACCURACY) {
+    beacons.add(bleacon);
+  } else {
+    beacons.delete(bleacon);
+  }
+  console.log(beacons);
 });
 
 Bleacon.startScanning();
