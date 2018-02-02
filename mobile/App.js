09368245ee41d32fb3b313f9608619fc060b3639
Dominik Markiewicz <theresq@gmail.com>
2018-02-02 23:51:24 +0100

    [mobile] reformat

--- mobile/App.js
+++ mobile/App.js
@@ -16,80 +16,80 @@ const db = firebase.database();
 
 // Start detecting all iBeacons in the nearby
 export default class App extends React.Component {
-    state = { x: {} }
-    componentDidMount() {
-        try {
-            const granted = PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                {
-                    'title': 'Location Permission',
-                    'message': 'Activeev needs to access your location.'
-                }
-            )
-            console.log('here', granted);
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                console.log("Location Permitted")
-            } else {
-                console.log("Location permission denied")
-            }
-        } catch (err) {
-            console.warn(err)
+  state = { x: {} }
+  componentDidMount() {
+    try {
+      const granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Location Permission',
+          'message': 'Activeev needs to access your location.'
         }
-
-
-        Beacons.startRangingBeaconsInRegion("REGION1")
-            .then((e) => console.log("H4FDM succ" + e), (e) => console.error("H4FDM err" + e))
-            .then(() => {
-                DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
-                    console.log('Found beacons!', data.beacons)
-                    const nw = data.beacons.map(b => [`${b.uuid}:${b.major}:${b.minor}`, b])
-                        .reduce((agg, v) => { agg[v[0]] = v[1]; return agg }, {})
-
-                    this.setState({
-                        x: {
-                            ...this.state.x,
-                            ...nw
-                        }
-                    }
-                    );
-                    DeviceEventEmitter.addListener('regionDidEnter', (d) => this.setState({ y: d }))
-                    DeviceEventEmitter.addListener('regionDidExit', (d) => this.setState({ z: d }))
-                })
-
-                db.goOnline()
-
-                db.ref(`products`).once('value')
-                    .then(snapshot => {
-                        const val = snapshot.val();
-                        this.setState({ z: val })
-                    })
-            })
+      )
+      console.log('here', granted);
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Location Permitted")
+      } else {
+        console.log("Location permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
     }
 
-    componentWillUnmount() {
-        Beacons.stopRangingBeaconsInRegion("REGION1")
-        db.goOffline()
-    }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start dupa 123 working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
-                <Text>x: {JSON.stringify(this.state.x, null, 2)}</Text>
-                <Text>y: {JSON.stringify(this.state.y, null, 2)}</Text>
-                <Text>z: {JSON.stringify(this.state.z, null, 2)}</Text>
-            </View>
-        );
-    }
+    Beacons.startRangingBeaconsInRegion("REGION1")
+      .then((e) => console.log("H4FDM succ" + e), (e) => console.error("H4FDM err" + e))
+      .then(() => {
+        DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
+          console.log('Found beacons!', data.beacons)
+          const nw = data.beacons.map(b => [`${b.uuid}:${b.major}:${b.minor}`, b])
+            .reduce((agg, v) => { agg[v[0]] = v[1]; return agg }, {})
+
+          this.setState({
+            x: {
+              ...this.state.x,
+              ...nw
+            }
+          }
+          );
+          DeviceEventEmitter.addListener('regionDidEnter', (d) => this.setState({ y: d }))
+          DeviceEventEmitter.addListener('regionDidExit', (d) => this.setState({ z: d }))
+        })
+
+        db.goOnline()
+
+        db.ref(`products`).once('value')
+          .then(snapshot => {
+            const val = snapshot.val();
+            this.setState({ z: val })
+          })
+      })
+  }
+
+  componentWillUnmount() {
+    Beacons.stopRangingBeaconsInRegion("REGION1")
+    db.goOffline()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start dupa 123 working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>x: {JSON.stringify(this.state.x, null, 2)}</Text>
+        <Text>y: {JSON.stringify(this.state.y, null, 2)}</Text>
+        <Text>z: {JSON.stringify(this.state.z, null, 2)}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
