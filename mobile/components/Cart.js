3162c8532091fd75a0dcb3b55a63e146264d36be
Mateusz Sikora <mateusz.sikora@nokia.com>
2018-02-03 02:03:32 +0100

    product list

--- mobile/components/Cart.js
+++ mobile/components/Cart.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import { Container, Header, Content, List, ListItem, Thumbnail, Text, Body } from 'native-base';
+import {Container, Header, Content, List, ListItem, Thumbnail, Text, Body} from 'native-base';
+import firebase from 'react-native-firebase';
+
+const db = firebase.database();
 
 export default class Cart extends React.Component {
 
+  state = {products: {}};
+
+  componentDidMount() {
+    db.ref(`products`).once('value')
+      .then(snapshot => {
+        const val = snapshot.val();
+        this.setState({products: val});
+      })
+  }
+
+  renderProducts(product) {
+    return (
+      <ListItem key={product.id}>
+        <Thumbnail square size={80} source={{uri: product.imageUrl}}/>
+        <Body>
+        <Text>{product.name}</Text>
+        <Text note>{product.description}</Text>
+        </Body>
+      </ListItem>
+    );
+  }
+
   render() {
+    const products = this.state.products;
+    const productKeys = Object.keys(products);
+
     return (
       <Container>
-        <Header />
+        <Header/>
         <Content>
           <List>
-            <ListItem>
-              <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
-              <Body>
-              <Text>Sankhadeep</Text>
-              <Text note>Its time to build a difference . .</Text>
-              </Body>
-            </ListItem>
+            {productKeys.map(key => this.renderProducts(products[key]))}
           </List>
         </Content>
       </Container>
