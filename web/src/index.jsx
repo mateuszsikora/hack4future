5bfa988aa8f62207235653cb852cb73affa9d7fa
Jakub Stasiak <kubafr2@gmail.com>
2018-02-03 07:16:33 +0100

    removed web

--- web/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-
-import App from './App';
-
-render(<App />, document.querySelector('.root'));
