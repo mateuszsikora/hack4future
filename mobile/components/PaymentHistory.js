4bb019e516db1ff4725d81bb1e59d4b797e5c0d9
Mateusz Sikora <mateusz.sikora@nokia.com>
2018-02-03 07:57:23 +0100

    fix table

--- mobile/components/PaymentHistory.js
+++ mobile/components/PaymentHistory.js
@@ -3,17 +3,30 @@ import {Text} from 'react-native';
 import {Container, Header, Body, Content, Title} from 'native-base';
 import {Table, Row, Rows} from 'react-native-table-component';
 
+import {web3, getAccount} from '../web3';
+import firebase from 'react-native-firebase';
+
+const db = firebase.database();
+
 export default class PaymentHistory extends React.Component {
+  state = {transactions: []};
+
+  componentDidMount() {
+    const account = getAccount();
+    db.ref(`transactions/${account.address}`)
+      .once('value')
+      .then(snapshot => {
+        const value = snapshot.val() || {};
+        const transactions = Object.keys(value).map(key => {
+          const transaction = value[key];
+          return [transaction.time, transaction.price, key];
+        });
+        this.setState({transactions});
+      });
+  }
 
   render() {
     const tableHead = ['Date', 'Amount', 'Transaction hash'];
-
-    const tableData = [
-      ['1', '2', '3',],
-      ['a', 'b', 'c',],
-      ['1', '2', '3',],
-      ['a', 'b', 'c',]
-    ];
 
     return (
       <Container>
@@ -25,7 +38,7 @@ export default class PaymentHistory extends React.Component {
         <Content>
           <Table>
             <Row data={tableHead} style={styles.head} textStyle={styles.headText}/>
-            <Rows data={tableData} style={styles.row} textStyle={styles.text}/>
+            <Rows data={this.state.transactions} style={styles.row} textStyle={styles.text}/>
           </Table>
         </Content>
       </Container>
