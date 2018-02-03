aaf51a50788ee34fd8d53544539b2431437a70fd
Mateusz Sikora <mateusz.sikora@nokia.com>
2018-02-03 07:26:41 +0100

    fixes

--- gate/src/index.js
+++ gate/src/index.js
@@ -27,50 +27,49 @@ function calculatePrice(beaconsSet) {
 const doPayments = _.debounce(
   () => {
     console.log("payments start!")
-    const bcWithHoldPromises = Array.from(holds).map(h=>{
+    const bcWithHoldPromises = Array.from(holds).map(h => {
       console.log('hold', h)
       return Device.findById(h.id).then((d) => [h, d])
     })
-    const bcWithHold = Promise.all(bcWithHoldPromises).
-      then(dt => {
-        console.log(dt);
-        return dt.reduce((agg, [b, d]) => {
-          console.log(b, d)
-          agg[d.account] = agg[d.account] || []
-          agg[d.account].push(b)
-          return agg
-        }, {})
-      }).then(receiptData => {
-        console.log("To pay: ", JSON.stringify(receiptData, null, 2))
-        const d = Object.entries(receiptData).map(([account, data])=>{
-          return calculatePrice(data).then((o)=> ({...o, account}));
-        })
+    const bcWithHold = Promise.all(bcWithHoldPromises).then(dt => {
+      console.log(dt);
+      return dt.reduce((agg, [b, d]) => {
+        console.log(b, d)
+        agg[d.account] = agg[d.account] || []
+        agg[d.account].push(b)
+        return agg
+      }, {})
+    }).then(receiptData => {
+      console.log("To pay: ", JSON.stringify(receiptData, null, 2))
+      const d = Object.entries(receiptData).map(([account, data]) => {
+        return calculatePrice(data).then((o) => ({...o, account}));
+      })
 
-        return Promise.all(d).then((t)=> {
-          Promise.all(
-            t.map(({account, products, amount })=>{
+      return Promise.all(d).then((t) => {
+        Promise.all(
+          t.map(({account, products, amount}) => {
             const o = new Order(account, products);
             return o.save().then(oo => {
               console.log(account, o.id, amount);
-              eth.sell(account, o.id, amount+"");
+              eth.sell(account, o.id, amount + "");
             })
           }))
 
-        })
       })
+    })
   }, 1000)
 
 Bleacon.on('discover', (bleacon) => {
   if (bleacon.accuracy < MAX_ACCURACY) {
     beacons.add(bleacon);
-    Hold.findByUUID(beacon).then(h=> {
-      if(h){
+    Hold.findByUUID(beacon.uuid).then(h => {
+      if (h) {
         holds.add(h);
         doPayments();
       }
     });
   }
-  calculatePrice(beacons).then(({ amount }) => console.log(amount))
+  calculatePrice(beacons).then(({amount}) => console.log(amount))
 });
 
-Bleacon.startScanning(console.error);
+Bleacon.startScanning();
